54bbf17aadc7ef58672ca2c2528acfa18658bdff
Aadesh Agarwal <aadesh.agarwal8888@gmail.com>
2021-05-18 14:47:22 +0530

    bug

--- src/Components/config.js
+++ src/Components/config.js
@@ -1,7 +1,7 @@
 var config = {
   // baseUrl: "http://localhost:9090"
   //baseUrl: "http://plasmadonation-env.eba-bhpbs99a.us-east-2.elasticbeanstalk.com"
-  baseUrl: "https://plasma-donation-server.herokuapp.com/"
+  baseUrl: "https://plasma-donation-server.herokuapp.com"
 };
 
 export default config;
